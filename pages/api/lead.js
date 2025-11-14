9ac1a6427263efa2bd96b0fb711976bbcadb3ee4
suellensilva26 <suellensilva.empresa@gmail.com>
2025-11-14 08:15:48 -0300

    Change API endpoint to Formspree

--- pages/api/lead.js
+++ pages/api/lead.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const response = await fetch('https://script.google.com/macros/s/AKfycbw1khziBb7wp3Y59DFZgCwSTd93w6jDXF2P4d8z1OcElZH1fgjvD3_zcysz5lTwBpqDww/exec', {
+    const response = await fetch('https://formspree.io/f/mvgdzwvy', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(req.body),
